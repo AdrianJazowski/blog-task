27ac09682fc4a6c59a769b132a0098c8de1da014
Adrian Jazowski <adrian.jazowski@gmail.com>
2021-07-16 19:17:37 +0200

    creating the main app.js component and styles for it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { getPostFromApiAction } from "./actions";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { routes } from "./routes";
+import Home from "./views/home/Home";
+import SinglePost from "./views/singlePost/SinglePost";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  const getPosts = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((respone) => respone.json())
+      .then((data) => {
+        dispatch(getPostFromApiAction(data));
+      });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path={routes.home} component={Home} />
+        <Route path={routes.singlePost} component={SinglePost} />
+      </Switch>
+      <div class="circle1"></div>
+      <div class="circle2"></div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
