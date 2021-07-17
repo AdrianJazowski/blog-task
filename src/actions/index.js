c95f589b378c04009fb527bcfc4459258549b9d4
Adrian Jazowski <adrian.jazowski@gmail.com>
2021-07-17 11:08:04 +0200

    ordering the number of actions in actions.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,6 @@ export const addOrRemovePostToLiked = (id) => ({
   type: actionsTypes.LIKE_OR_UNLIKE_POST,
   payload: id,
 });
-export const addToTemporaryCommentBoard = (comments) => ({
-  type: actionsTypes.ADD_TO_TEMPORARY_COMMENT_BOARD,
-  payload: comments,
-});
 export const addOrRemoveCommentToLiked = (id) => ({
   type: actionsTypes.LIKE_OR_UNLIKE_COMMENT,
   payload: id,
